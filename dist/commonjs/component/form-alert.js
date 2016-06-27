b84e5652e61d43d90376cad70f179366ca283a80
Flávio Alves <flavio.alves@wemake.pt>
2016-06-27 14:44:41 +0100

    Refactored form-alert custom element.

--- dist/commonjs/component/form-alert.js
+++ dist/commonjs/component/form-alert.js
@@ -5,7 +5,9 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.FormAlert = undefined;
 
-var _dec, _dec2, _dec3, _class, _desc, _value, _class2, _descriptor;
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _dec, _dec2, _dec3, _dec4, _dec5, _dec6, _dec7, _class, _desc, _value, _class2, _descriptor;
 
 var _aureliaFramework = require("aurelia-framework");
 
@@ -56,11 +58,38 @@ function _initializerWarningHelper(descriptor, context) {
     throw new Error('Decorating class property failed. Please ensure that transform-class-properties is enabled.');
 }
 
-var FormAlert = exports.FormAlert = (_dec = (0, _aureliaFramework.customElement)('form-alert'), _dec2 = (0, _aureliaViewManager.resolvedView)('spoonx/form', 'form-alert'), _dec3 = (0, _aureliaFramework.bindable)({ defaultBindingMode: _aureliaFramework.bindingMode.twoWay }), _dec(_class = _dec2(_class = (_class2 = function FormAlert() {
-    _classCallCheck(this, FormAlert);
+var FormAlert = exports.FormAlert = (_dec = (0, _aureliaFramework.customElement)('form-alert'), _dec2 = (0, _aureliaViewManager.resolvedView)('spoonx/form', 'form-alert'), _dec3 = (0, _aureliaFramework.bindable)({ defaultBindingMode: _aureliaFramework.bindingMode.twoWay }), _dec4 = (0, _aureliaFramework.computedFrom)('alert'), _dec5 = (0, _aureliaFramework.computedFrom)('alert'), _dec6 = (0, _aureliaFramework.computedFrom)('alert'), _dec7 = (0, _aureliaFramework.computedFrom)('alert'), _dec(_class = _dec2(_class = (_class2 = function () {
+    function FormAlert() {
+        _classCallCheck(this, FormAlert);
 
-    _initDefineProp(this, "alert", _descriptor, this);
-}, (_descriptor = _applyDecoratedDescriptor(_class2.prototype, "alert", [_dec3], {
+        _initDefineProp(this, "alert", _descriptor, this);
+    }
+
+    _createClass(FormAlert, [{
+        key: "attributes",
+        get: function get() {
+            return this.alert.attributes || '';
+        }
+    }, {
+        key: "iconAttributes",
+        get: function get() {
+            console.log(this.alert.icon.attributes);
+            return this.alert.icon.attributes || '';
+        }
+    }, {
+        key: "message",
+        get: function get() {
+            return this.alert.message || '';
+        }
+    }, {
+        key: "details",
+        get: function get() {
+            return this.alert.details || '';
+        }
+    }]);
+
+    return FormAlert;
+}(), (_descriptor = _applyDecoratedDescriptor(_class2.prototype, "alert", [_dec3], {
     enumerable: true,
     initializer: null
-})), _class2)) || _class) || _class);
+}), _applyDecoratedDescriptor(_class2.prototype, "attributes", [_dec4], Object.getOwnPropertyDescriptor(_class2.prototype, "attributes"), _class2.prototype), _applyDecoratedDescriptor(_class2.prototype, "iconAttributes", [_dec5], Object.getOwnPropertyDescriptor(_class2.prototype, "iconAttributes"), _class2.prototype), _applyDecoratedDescriptor(_class2.prototype, "message", [_dec6], Object.getOwnPropertyDescriptor(_class2.prototype, "message"), _class2.prototype), _applyDecoratedDescriptor(_class2.prototype, "details", [_dec7], Object.getOwnPropertyDescriptor(_class2.prototype, "details"), _class2.prototype)), _class2)) || _class) || _class);
--- src/component/form-alert.js
+++ src/component/form-alert.js
@@ -6,4 +6,25 @@ import {resolvedView} from "aurelia-view-manager";
 export class FormAlert {
     @bindable({defaultBindingMode: bindingMode.twoWay})
     alert;
+    
+    @computedFrom('alert')
+    get attributes() {
+        return this.alert.attributes || '';
+    }
+
+    @computedFrom('alert')
+    get iconAttributes() {
+        console.log(this.alert.icon.attributes);
+        return this.alert.icon.attributes || '';
+    }
+
+    @computedFrom('alert')
+    get message() {
+        return this.alert.message || '';
+    }
+
+    @computedFrom('alert')
+    get details() {
+        return this.alert.details || '';
+    }
 }
